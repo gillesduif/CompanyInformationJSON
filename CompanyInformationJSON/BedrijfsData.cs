c0c44957d8021e2680cde59ad7df916c9d4095cb
gillesduif <60394236+gillesduif@users.noreply.github.com>
2022-01-28 11:15:34 +0100

    first

---
+++ CompanyInformationJSON/BedrijfsData.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompanyInformationJSON
+{
+    using System;
+    using System.Collections.Generic;
+
+    using System.Globalization;
+    using System.Text.Json.Serialization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    public partial class BedrijfsData
+    {
+        [JsonProperty("enterprise")]
+        public Enterprise Enterprise { get; set; }
+
+        [JsonProperty("denomination")]
+        public Denomination Denomination { get; set; }
+
+        [JsonProperty("kpis")]
+        public Kpi[] Kpis { get; set; }
+
+        [JsonProperty("establishments")]
+        public Establishment[] Establishments { get; set; }
+
+        [JsonProperty("activities")]
+        public Activity[] Activities { get; set; }
+
+        [JsonProperty("contacts")]
+        public object[] Contacts { get; set; }
+    }
+
+    public partial class Activity
+    {
+        [JsonProperty("ActivityGroup")]
+        public object ActivityGroup { get; set; }
+
+        [JsonProperty("NaceCodeLabel")]
+        public string NaceCodeLabel { get; set; }
+
+        [JsonProperty("NaceCode")]
+      
+        public long NaceCode { get; set; }
+
+        [JsonProperty("Classification")]
+        public string Classification { get; set; }
+    }
+
+    public partial class Denomination
+    {
+        [JsonProperty("Lei")]
+        public object Lei { get; set; }
+
+        [JsonProperty("Denomination")]
+        public string DenominationDenomination { get; set; }
+
+        [JsonProperty("Language")]
+        public string Language { get; set; }
+
+        [JsonProperty("TypeOfDenomination")]
+        public string TypeOfDenomination { get; set; }
+    }
+
+    public partial class Enterprise
+    {
+        [JsonProperty("EnterpriseNumber")]
+        public string EnterpriseNumber { get; set; }
+
+        [JsonProperty("Status")]
+        public string Status { get; set; }
+
+        [JsonProperty("StatusColor")]
+        public string StatusColor { get; set; }
+
+        [JsonProperty("FiscalYearEnd")]
+        public DateTimeOffset FiscalYearEnd { get; set; }
+
+        [JsonProperty("Employees")]
+        public string Employees { get; set; }
+
+        [JsonProperty("JuridicalSituation")]
+        public string JuridicalSituation { get; set; }
+
+        [JsonProperty("TypeOfEnterprise")]
+        public string TypeOfEnterprise { get; set; }
+
+        [JsonProperty("JuridicalForm")]
+        public string JuridicalForm { get; set; }
+
+        [JsonProperty("StartDate")]
+        public string StartDate { get; set; }
+
+        [JsonProperty("address")]
+        public Address Address { get; set; }
+    }
+
+    public partial class Address
+    {
+        [JsonProperty("TypeOfAddress")]
+        public string TypeOfAddress { get; set; }
+
+        [JsonProperty("Zipcode")]
+       
+        public long Zipcode { get; set; }
+
+        [JsonProperty("MunicipalityNL")]
+        public string MunicipalityNl { get; set; }
+
+        [JsonProperty("StreetNL")]
+        public string StreetNl { get; set; }
+
+        [JsonProperty("HouseNumber")]
+      
+        public long HouseNumber { get; set; }
+
+        [JsonProperty("ExtraAddressInfo")]
+        public string ExtraAddressInfo { get; set; }
+
+        [JsonProperty("DateStrikingOff")]
+        public string DateStrikingOff { get; set; }
+    }
+
+    public partial class Establishment
+    {
+        [JsonProperty("EstablishmentNumber")]
+        public string EstablishmentNumber { get; set; }
+
+        [JsonProperty("StartDate")]
+        public string StartDate { get; set; }
+
+        [JsonProperty("denomination")]
+        public Denomination Denomination { get; set; }
+
+        [JsonProperty("address")]
+        public Address Address { get; set; }
+    }
+
+    public partial class Kpi
+    {
+        [JsonProperty("Label")]
+        public string Label { get; set; }
+
+        [JsonProperty("Display")]
+        public bool Display { get; set; }
+
+        [JsonProperty("Years")]
+        public Dictionary<string, Year> Years { get; set; }
+        public override string ToString()
+        {
+            foreach (string key in Years.Keys)
+            {
+                return key;
+            }
+            return "OOOOPS";
+        }
+    }
+
+    public partial class Year
+    {
+        [JsonProperty("Amount")]
+        public string Amount { get; set; }
+
+        [JsonProperty("Reference", NullValueHandling = NullValueHandling.Ignore)]
+        public string Reference { get; set; }
+
+        [JsonProperty("ChangePct")]
+        public long? ChangePct { get; set; }
+    }
+
+
+
+ 
+}
---
+++ CompanyInformationJSON/Program.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+namespace CompanyInformationJSON
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geef het btw nummer in dat u wilt opzoeken:");
+            string btwnr = Console.ReadLine();
+            //string btwnr = "0866.039.061";
+            var url = $"https://robofin.be/fiche/api.enterprise.php?a=EnterpriseData&VAT={btwnr}";
+
+            var httpRequest = (HttpWebRequest)WebRequest.Create(url);
+
+            httpRequest.Accept = "application/json";
+
+            var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+                var bedrijfsData = JsonConvert.DeserializeObject<BedrijfsData>(result);
+                List<Kpi> kpilijst = new List<Kpi>();
+               // var first = bedrijfsData.Kpis.yFirst();
+
+                foreach (object i in bedrijfsData.Kpis)
+                {
+                    kpilijst.Add(((Kpi)i));
+                }
+                int counter = 0;
+                foreach(Kpi kpi in kpilijst)
+                {
+                    if(counter == 0)
+                    {
+                        //Console.WriteLine("Profit/loss" + kpi.Years["2020-12-31"].Amount.ToString());
+                        counter++;
+                    }
+                    else  if (counter == 1)
+                    {
+                    //    Console.WriteLine(kpi.Years["2020-12-31"].Amount.ToString());
+                    }
+                  // Console.WriteLine(kpi.Years["2020-12-31"].Amount.ToString());
+                }
+            
+                Console.WriteLine(bedrijfsData.Denomination.DenominationDenomination);
+                Console.WriteLine(result);
+            }
+
+            Console.WriteLine(httpResponse.StatusCode);
+        }
+    }
+}
